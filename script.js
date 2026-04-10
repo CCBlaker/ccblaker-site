1c5d6c89b61ba55511576a8be99126940e9ab7c3
Marigold <111760235+LiterallyAFlower@users.noreply.github.com>
2026-04-09 21:50:35 -0400

    added all needed functionality, focus on style next.

--- script.js
+++ script.js
@@ -1,21 +1,82 @@
 function loadCategory(categorySlug) {
-    fetch('data/projects.json')
+    fetch('/ccblaker-site/data/projects.json')
         .then(res => res.json())
+        // .then(text => {
+        //     console.log(text);
+        // })
         .then(data => {
             const category = data.categories.find(c => c.slug === categorySlug);
             const container = document.getElementById('projects');
 
             category.projects.forEach(project => {
-                const div = document.createElement('div');
+                fetch('/ccblaker-site/' + project.path + 'data.json')
+                  .then(res => res.json())
+                  .then(projData => {
+                    const div = document.createElement('div');
 
-                div.innerHTML = `
-                    <h3>${project.title}</h3>
-                    <a href="project.html?proj=${project.slug}">
-                        View Project
-                    </a>
-                `;
+                    const mediaPath = '/ccblaker-site/' + project.path + projData.media;
 
-                container.appendChild(div);
+                    div.innerHTML = `
+                        <div class="card">
+                            
+                            <div class="container">
+                                
+                                <a href="/ccblaker-site/project.html?proj=${project.slug}">
+                                    <img src="${mediaPath}" alt="${project.alt} class="thumbnail"/>
+                                </a>
+                            </div>
+                        </div>
+                        
+                    `;
+
+                    container.appendChild(div);
+                  });
             });
         });
+}
+
+
+function loadProject() {
+    const params = new URLSearchParams(window.location.search);
+    const slug = params.get('proj');
+
+    if(!slug){
+        console.error("No project slug in URL");
+    }
+
+    fetch('/ccblaker-site/data/projects.json')
+      .then(res => res.json())
+      .then(data => {
+        let foundProject;
+
+        data.categories.forEach(cat => {
+            cat.projects.forEach(p => {
+                if (p.slug === slug) {
+                    foundProject = p;
+                }
+            });
+        });
+
+        const basePath = '/ccblaker-site/' + foundProject.path;
+
+        fetch(basePath + 'data.json')
+          .then(res => res.json())
+          .then(projData => {
+            const container = document.getElementById('project');
+
+            let mediaHTML = '';
+
+            if(projData.type === 'video') {
+                mediaHTML = `<video controls src="${basePath + projData.media}" />`;
+            } else {
+                mediaHTML = `<img src="${basePath + projData.media}" />`;
+            }
+
+            container.innerHTML = `
+                <h1>${projData.title}</h1>
+                ${mediaHTML}
+                <p>${projData.description}</p>
+            `;
+          });
+      });
 }
